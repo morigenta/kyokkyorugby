d19b6cabfa240ee6ac2de8769a861e9ac7e715d0
Genta Mori <xiantasen@gmail.com>
2023-03-19 00:15:10 +0900

    いったん撤退

---
+++ public/assets/responsive.js
@@ -0,0 +1,8 @@
+$(function(){
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $('.s_01 .accordion_one .accordion_header').click(function(){
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next('.accordion_inner').slideToggle();
+    $(this).toggleClass("open");
+  });
+});
